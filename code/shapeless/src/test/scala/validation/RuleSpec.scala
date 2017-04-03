d6fa0de68b9e5fad67d855c61655e8bdf11c84e8
Dave Gurnell <dave@untyped.com>
2017-04-03 15:20:48 +0100

    Fleshed out unit test

--- code/shapeless/src/test/scala/validation/RuleSpec.scala
+++ code/shapeless/src/test/scala/validation/RuleSpec.scala
@@ -49,10 +49,20 @@ class RuleSpec extends FreeSpec with Matchers {
   }
 
   "field" in {
-    val rule = Rule.pass[IceCream].field('cherries)(Rule.gte(0))
+    val rule = Rule.pass[IceCream]
+      .field('cherries)(Rule.gte(0))
+      .field('name)(Rule.nonEmpty)
 
     rule(IceCream("Sundae", 1, false)) should be(Nil)
-    rule(IceCream("Sundae", -1, false)) should be(List(Error("Must be >= 0", "cherries" :: Nil)))
+
+    rule(IceCream("Sundae", -1, false)) should be(List(
+      Error("Must be >= 0", "cherries" :: Nil)
+    ))
+    
+    rule(IceCream("", -1, false)) should be(List(
+      Error("Must be >= 0", "cherries" :: Nil),
+      Error("Must be non-empty", "name" :: Nil)
+    ))
   }
 
 }
